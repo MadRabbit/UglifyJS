7661b953cc13341a8f0e0bf5a78d1c33f5e7a466
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2011-03-20 15:57:08 +0300

    Adding an one-call compress function for Node

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-exports.parser = require("./lib/parse-js");
-exports.uglify = require("./lib/process");
+var jsp = require("./lib/parse-js");
+var pro = require("./lib/process");
+
+/**
+ * Just a simple one-call compressor
+ *
+ * @param {string} source
+ * @return {string} build
+ */
+function compress(source) {
+  var ast = jsp.parse(source);
+
+  ast = pro.ast_mangle(ast);
+  ast = pro.ast_squeeze(ast);
+
+  return pro.gen_code(ast);
+}
+
+exports.parser   = jsp;
+exports.uglify   = pro;
+exports.compress = compress;
